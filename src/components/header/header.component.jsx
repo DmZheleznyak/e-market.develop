ee721380ae4da2db7c3aa02e1886fe55d57ef0a2
DmZheleznyak <11lione1991@gmail.com>
2021-05-17 07:23:37 +0300

    created store, implement userReduser and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import logo from '../../assets/knitting.svg';
 
 import { auth } from './../../firebase/firebase.utils';
@@ -31,4 +33,8 @@ const Header = ({ currentUser }) => (
     </header>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
